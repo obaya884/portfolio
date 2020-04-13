e5d8ec2d35bb13e85a2a50ead9b988bc13a1634c
obaya884 <taku3.884@gmail.com>
2020-04-14 02:01:34 +0900

    re-place ogp image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Helmet } from "react-helmet";
 import Sidebar from "./components/sidebar";
 import Top from "./components/top";
 import About from "./components/about";
@@ -18,29 +17,6 @@ library.add(fab, fas, far);
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>Portfolio2020 | Takumi Obayashi</title>
-        <meta property="og:title" content="Portfolio2020 | Takumi Obayashi" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://obaya884.github.io/portfolio/#/"
-        />
-        <meta property="og:image" content="./images/ogp_image.png" />
-        <meta
-          property="og:site_name"
-          content="Portfolio2020 | Takumi Obayashi"
-        />
-        <meta
-          property="og:description"
-          content="大林拓実のポートフォリオサイト。Reactで作成。自身の経歴や作品を紹介しています。"
-        />
-        <meta property="og:locale" content="ja_JP" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@obaya884" />
-        <meta name="twitter:creator" content="@obaya884" />
-      </Helmet>
       <HashRouter>
         <Sidebar></Sidebar>
         <Route path="/">
